73e14d80e7e797f00fca283e893f8fbd975dba1a
AhmedJr-wq <abulfail.rufai@amalitech.com>
2023-06-07 11:09:27 +0000

    abstract classes completed

--- classes/abstractClasses.js
+++ classes/abstractClasses.js
@@ -1,2 +1,55 @@
 //Abstract classes by default cannot be instantiated directly and is meant to serve as a base or blueprint for other classes. 
-//Abstract classes are designed to be extended by other classes which can inherit therir properties, methods, and behavior.
+//Abstract classes are designed to be extended by other classes which can inherit their properties, methods, and behavior.
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Shape = /** @class */ (function () {
+    function Shape() {
+    }
+    Shape.prototype.display = function () {
+        console.log("The area of this shape is ".concat(this.calculateArea()));
+    };
+    return Shape;
+}());
+var Circle = /** @class */ (function (_super) {
+    __extends(Circle, _super);
+    function Circle(radius) {
+        var _this = _super.call(this) || this;
+        _this.radius = radius;
+        return _this;
+        //It allows the subclass to inherit and initialize the properties and behavior defined in the superclass.
+    }
+    Circle.prototype.calculateArea = function () {
+        return Math.PI * Math.pow(this.radius, 2);
+    };
+    return Circle;
+}(Shape));
+var Rectangle = /** @class */ (function (_super) {
+    __extends(Rectangle, _super);
+    function Rectangle(width, height) {
+        var _this = _super.call(this) || this;
+        _this.width = width;
+        _this.height = height;
+        return _this;
+    }
+    Rectangle.prototype.calculateArea = function () {
+        return this.width * this.height;
+    };
+    return Rectangle;
+}(Shape));
+var circle = new Circle(5);
+circle.display(); // Output: "The area of this shape is 78.53981633974483"
+var rectangle = new Rectangle(4, 6);
+rectangle.display(); // Output: "The area of this shape is 24"
--- classes/abstractClasses.ts
+++ classes/abstractClasses.ts
@@ -1,2 +1,49 @@
 //Abstract classes by default cannot be instantiated directly and is meant to serve as a base or blueprint for other classes. 
-//Abstract classes are designed to be extended by other classes which can inherit therir properties, methods, and behavior.
+//Abstract classes are designed to be extended by other classes which can inherit their properties, methods, and behavior.
+
+abstract class Shape {
+    abstract calculateArea(): number;
+  
+    display() {
+      console.log(`The area of this shape is ${this.calculateArea()}`);
+    }
+  }
+  
+  class Circle extends Shape {
+
+    constructor(
+        private radius: number
+    ) {
+
+      super(); //the super() keyword is used within a subclass to call the constructor of its superclass. 
+      //It allows the subclass to inherit and initialize the properties and behavior defined in the superclass.
+
+    }
+  
+    calculateArea(): number {
+      return Math.PI * Math.pow(this.radius, 2);
+    }
+  }
+  
+  class Rectangle extends Shape {
+
+    constructor(
+        private width: number, 
+        private height: number
+    ) {
+
+      super();
+
+    }
+  
+    calculateArea(): number {
+      return this.width * this.height;
+    }
+  }
+  
+  const circle = new Circle(5);
+  circle.display(); // Output: "The area of this shape is 78.53981633974483"
+  
+  const rectangle = new Rectangle(4, 6);
+  rectangle.display(); // Output: "The area of this shape is 24"
+  
