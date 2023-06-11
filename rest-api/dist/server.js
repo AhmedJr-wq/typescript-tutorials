84ea1adaac1c186631a7a6a78191f444b8182e7a
AhmedJr-wq <abulfail.rufai@amalitech.com>
2023-06-11 21:38:09 +0000

    adding dotenv file for supporting multiple environments

--- rest-api/dist/server.js
+++ rest-api/dist/server.js
@@ -1,13 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var dotenv = require("dotenv");
+var result = dotenv.config();
+if (result.error) {
+    console.log("Error loading environment variables, aborting...");
+    process.exit(1);
+}
+console.log(process.env.PORT);
 var express = require("express");
 var root_1 = require("./routes/root");
+var utils_1 = require("./utils");
 var app = express();
 function setupExpress() {
     app.route('/').get(root_1.root); //request handler. 'root' function generates http response
 }
 function startServer() {
-    app.listen(9000, function () {
+    var portArg = process.argv[2];
+    var port;
+    if ((0, utils_1.isInteger)(portArg)) {
+        port = parseInt(portArg);
+    }
+    if (!port) {
+        port = 9000;
+    }
+    app.listen(port, function () {
         console.log("HTTP REST API Server is now running at http://localhost:9000");
     });
 }
